39fbc8edd00b5b8ea8acb72ebdb031533eb9c208
MarahDeven <marahdeven@gmail.com>
2022-03-10 10:54:29 -0500

    Set up file for app, deleted unnecessary files, set up bootstrap, hosted to netlify

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import dictionary from "./dictionary.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={dictionary} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
