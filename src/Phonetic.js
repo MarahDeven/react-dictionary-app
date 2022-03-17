ed6e8f0b406f2f4a64ff768fa29ebd93b1b34f72
MarahDeven <marahdeven@gmail.com>
2022-03-17 10:01:06 -0400

    Added a phonetics component, also did trial and error to create an audio button

---
+++ src/Phonetic.js
@@ -0,0 +1,14 @@
+import React from "react";
+import audio from "./audio.png";
+
+export default function Phonetic(props) {
+  console.log(props.phonetic);
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        <img src={audio} className="audio-icon img-fluid" alt="audio" />
+      </a>
+      {props.phonetic.text}
+    </div>
+  );
+}
