3fd1804ab44bcd21593bf27e6be9861829823acd
MarahDeven <marahdeven@gmail.com>
2022-03-21 09:46:03 -0400

    created keyword to automatically search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,42 +3,56 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyWord);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search}>
-          <input
-            type="search"
-            autoFocus="false"
-            onChange={handleKeywordChange}
-          />
-        </form>
-        <div className="hint">
-          Suggested Words: Pretentious, Ubiquitous, Cynical, Apathetic,
-          Conundrum, Albeit, Ambiguous, Integrity, etc...
-        </div>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              autoFocus="false"
+              onChange={handleKeywordChange}
+            />
+          </form>
+          <div className="hint">
+            Suggested Words: Pretentious, Ubiquitous, Cynical, Apathetic,
+            Conundrum Albeit, Integrity, etc...
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
